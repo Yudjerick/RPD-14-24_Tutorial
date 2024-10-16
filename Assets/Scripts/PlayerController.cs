3d89a61504b9abadffae84b8a0e6bc8bc11b402d
Yudjerick <denispetrenko2004@gmail.com>
2024-10-16 17:04:58 +0300

    Update code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     public Rigidbody rb;
     public float jumpForce = 500f;
+    public float rotationSpeed;
     Vector3 moveDirection;
     bool mustJump = false;
     bool isGrounded = true;
@@ -20,11 +21,20 @@ public class PlayerController : MonoBehaviour
 
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveZ = Input.GetAxisRaw("Vertical");
+
+        float mouseMovX = Input.GetAxis("Mouse X");
+        float mouseMovY = Input.GetAxis("Mouse Y");
+
+        print(mouseMovX);
+        print(mouseMovY);
+
+
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             mustJump = true;
         }
 
+        transform.rotation = Quaternion.Euler(0, mouseMovX * Time.deltaTime * rotationSpeed, 0) * transform.rotation;
         moveDirection = new Vector3(moveX, 0, moveZ).normalized * speed;
 
         
@@ -37,24 +47,16 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(Vector3.up * jumpForce);
             mustJump = false;
         }
-        rb.velocity = new Vector3(moveDirection.x, rb.velocity.y, moveDirection.z);
+        rb.velocity = transform.rotation * new Vector3(moveDirection.x, rb.velocity.y, moveDirection.z);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true;
-        }
-        
+        isGrounded = true;
     }
 
-    private void OnCollisionExit(Collision collision)
+    private void OnTriggerExit(Collider other)
     {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = false;
-        }
-        
+        isGrounded = false;
     }
 }
